b939841f725232ecc877c88524b22c632e5d48e0
okk-bia <oleksandr.kovbasiuk@biolinscientific.com>
2025-04-06 17:38:53 +0300

    Enabled-disabled Start button

--- timer_component/timer_controller.py
+++ timer_component/timer_controller.py
@@ -15,6 +15,7 @@ class TimerController:
         self._timer_model.event_on_exception = self.show_exception
         self._ui = TimerComponent()
         self._ui.btn_start.bind(on_press=self.start_timer)
+        self._ui.btn_start.bind(on_press=self.disable_button)
         self._ui.build()
 
     def start_timer(self, *args):
@@ -29,9 +30,17 @@ class TimerController:
         except ValueError as value_error:
             LOG.exception(value_error)
             self._ui.lbl_timer.text = f"Invalid user input: {value_error}"
+            self.enable_button()
         except Exception as ex:
             LOG.exception(ex)
             self._ui.lbl_timer.text = f"Unexpected error: {ex}"
+            self.enable_button()
+
+    def disable_button(self, *args):
+        self._ui.btn_start.disabled = True
+
+    def enable_button(self, *args):
+        self._ui.btn_start.disabled = False
 
     def update_remaining_time(self, *args):
         LOG.debug("update_remaining_time")
@@ -40,6 +49,7 @@ class TimerController:
     def timeout_reached(self, *args):
         LOG.debug("update_remaining_time")
         self._ui.lbl_timer.text = "Timeout"
+        self.enable_button()
 
     def check_interval(self, interval):
         if not (self._timer_model.min_limit <= interval <= self._timer_model.max_limit):
@@ -49,6 +59,7 @@ class TimerController:
 
     def show_exception(self, *args):
         self._ui.lbl_timer.text = self._timer_model.timer_exception
+        self.enable_button()
 
     @property
     def ui(self):
